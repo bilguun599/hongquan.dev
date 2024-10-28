93e42069805beab362432b9cfad601f668d3f8f2
QuanDH <hongquan.contact@gmail.com>
2024-10-28 14:53:41 +0700

    FIX FOOTER

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,24 +10,7 @@ const Footer = () => {
       <div className="flex items-center justify-center gap-1">
         <Typography className="flex items-center" variant="body3">
           <Copyright className="mr-1 inline-block h-4 w-4" />
-          {new Date().getFullYear()} |&nbsp;
-          <Link
-            noCustomization
-            externalLink
-            withUnderline
-            href={EXTERNAL_LINKS.FIGMA_FILE}
-          >
-            Designed
-          </Link>
-          &nbsp;and&nbsp;
-          <Link
-            noCustomization
-            externalLink
-            withUnderline
-            href={EXTERNAL_LINKS.GITHUB_REPO}
-          >
-            coded
-          </Link>
+          {new Date().getFullYear()} |&nbsp; | All rights reserved
         </Typography>
       </div>
     </footer>
